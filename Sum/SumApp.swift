25cde77420c0a3ff8d1a25ec60123a8ab2490d30
Dan Rudolf <danrudolf@outlook.com>
2023-08-02 14:07:21 -0500

    Initial Commit

---
+++ Sum/SumApp.swift
@@ -0,0 +1,17 @@
+//
+//  SumApp.swift
+//  Sum
+//
+//  Created by Dan Rudolf on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
